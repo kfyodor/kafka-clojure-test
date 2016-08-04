c802010d96f7c89bd3c4fc1afc164308b0f8c6fe
Theodore Konukhov <komolov.f@gmail.com>
2016-08-04 19:35:06 +0300

    clj->java avro

--- src/kafka_test/avro.clj
+++ src/kafka_test/avro.clj
@@ -2,9 +2,14 @@
   (:require [clojure.java.io :as io]
             [cheshire.core :as json]
             [camel-snake-kebab.core :refer [->kebab-case ->snake_case]])
-  (:import [org.apache.avro.generic GenericData GenericData$Record]
+  (:import [org.apache.avro.generic
+            GenericData
+            GenericData$Record
+            GenericData$Array
+            GenericData$Fixed
+            GenericData$EnumSymbol]
            [org.apache.avro.util Utf8]
-           [org.apache.avro Schema]))
+           [org.apache.avro Schema Schema$Type]))
 
 ;; this is test code: load all schemas into a Clojure map
 (def schemas
@@ -17,13 +22,95 @@
        (into {})
        (doall)))
 
-(defn- make-generic-record [^Schema schema record]
-  (reduce (fn [avro-record [k v]]
-            (let [key (-> k name ->snake_case)]
-              (.put avro-record key v)
-              avro-record))
-          (GenericData$Record. schema)
-          record))
+(defn- throw-invalid-type
+  [schema obj]
+  (throw (Exception. (format "Value `%s` cannot be cast to `%s` schema"
+                             (str obj)
+                             (.toString schema)))))
+
+(defn- bytes? [x]
+  (if (nil? x)
+    false
+    (-> x class .getComponentType (= Byte/TYPE))))
+
+(defn- clj->java
+  [schema obj]
+  (condp = (and (instance? Schema schema) (.getType schema))
+    Schema$Type/NULL
+    (if (nil? obj)
+      nil
+      (throw-invalid-type schema obj))
+
+    Schema$Type/INT
+    (if (and (number? obj) (<= Integer/MIN_VALUE obj Integer/MAX_VALUE))
+      (int obj)
+      (throw-invalid-type schema obj))
+
+    Schema$Type/LONG
+    (if (and (number? obj) (<= Long/MIN_VALUE obj Long/MAX_VALUE))
+      (long obj)
+      (throw-invalid-type schema obj))
+
+    Schema$Type/FLOAT
+    (if (float? obj)
+      (float obj)
+      (throw-invalid-type schema obj))
+
+    Schema$Type/DOUBLE
+    (if (float? obj)
+      (double obj)
+      (throw-invalid-type schema obj))
+
+    Schema$Type/BOOLEAN
+    (if (instance? Boolean obj) ;; boolean? added only in 1.9 :(
+      obj
+      (throw-invalid-type schema obj))
+
+    Schema$Type/STRING
+    (if (string? obj)
+      obj
+      (throw-invalid-type schema obj))
+
+    Schema$Type/BYTES
+    (if (bytes? obj)
+      obj
+      (throw-invalid-type schema obj))
+
+    Schema$Type/ARRAY ;; TODO Exception for complex type
+    (let [f (partial clj->java (.getElementType schema))]
+      (GenericData$Array. schema (map f obj)))
+
+    Schema$Type/FIXED
+    (if (and (bytes? obj) (= (count obj) (.getFixedSize schema)))
+      (GenericData$Fixed. schema obj)
+      (throw-invalid-type schema obj))
+
+    Schema$Type/ENUM
+    (let [enums (into #{} (.getEnumSymbols schema))]
+      (if (contains? enums obj)
+        (GenericData$EnumSymbol. schema obj)
+        (throw-invalid-type schema obj)))
+
+    Schema$Type/MAP ;; TODO Exception for complex type
+    (zipmap (map (comp name ->snake_case) (keys obj))
+            (map (partial clj->java (.getValueType schema))
+                 (vals obj)))
+
+    Schema$Type/UNION
+    nil
+
+    Schema$Type/RECORD
+    (reduce-kv
+     (fn [record k v]
+       (let [k (-> (name k) ->snake_case)
+             s (some-> (.getField schema k)
+                       (as-> f (.schema f)))]
+         (doto record
+           (.put k (clj->java (or s k) v)))))
+     (GenericData$Record. schema)
+     obj)
+
+    (throw (Exception. (format "Field `%s` is not in schema" schema)))))
 
 (defn make-message
   [entity-type value]
@@ -32,7 +119,7 @@
         record (make-generic-record schema value)]
     {:topic topic
      :key (str (:id value))
-     :value record}))
+     :value (clj->java schema value)}))
 
 (defn maybe-parse-string
   ;; todo: rename to parse-value
@@ -56,3 +143,7 @@
 (defn parse-avro-stream
   [stream]
   (map #(update-in % [:value] parse-message) stream))
+
+
+;; PRIMITIVE_TYPES = Set.new(%w[null boolean string bytes int long float double])
+;; NAMED_TYPES =     Set.new(%w[fixed enum record error])
